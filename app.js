fab6661a77e7dbc62b041bced2e1db9eb81027d9
Mohd Ahsan <mohdahsan@MacBook-Air-2.local>
2023-11-07 18:18:28 +0530

    docker files updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const log = require("./utils/logger");
 const app = express();
 var https = require('https');
+const http = require("http")
 const config = require("./utils/config.js");
 const router = require("./routes/route");
 
@@ -332,7 +333,7 @@ async function startUp(file) {
   };
   app.use(express.json());
   const logger = log.init();
-  https.createServer(options, app).listen(server.port, () => {
+  http.createServer(options, app).listen(server.port, () => {
     logger.info(`This app is running on port number : ${server.port}`);
   });
   // app.listen(server.port, () => {
